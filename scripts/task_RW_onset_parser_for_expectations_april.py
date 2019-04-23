00ad211c367a90ccb8e129ca42800053da12e35e
jennysadler <jennyrgilb@gmail.com>
2019-04-23 15:25:10 -0400

    updating choice parser and jupyter

--- scripts/task_RW_onset_parser_for_expectations_april.py
+++ scripts/task_RW_onset_parser_for_expectations_april.py
@@ -17,11 +17,11 @@ Reward: sweet taste = 1; bitter taste = 0
 """
 import os
 import glob
-import pandas as pd
+#import pandas as pd
 
 handles=[]
 
-basepath='/Users/jennygilbert/Google Drive/NIBL/Projects/18-0417 BeveL/data/task_behavior/task_logs'
+basepath='/Users/jennygilbert/Documents/bevel_choice_computations/task_logs'
 os.chdir(basepath)
 
 
@@ -30,8 +30,8 @@ ignore = ['DATA 	Keypress: o','Level post injecting via pump at address']
 for file in glob.glob(os.path.join(basepath,'bevel_*.log')):
     print(file)
 
-    sub=file.split('/')[10].split('_')[1].split('l')[1]
-    run=file.split('/')[10].split('_')[2]
+    sub=file.split('/')[6].split('_')[1].split('l')[1]
+    run=file.split('/')[6].split('_')[2]
     print([sub,run])
     
 
@@ -40,6 +40,7 @@ for file in glob.glob(os.path.join(basepath,'bevel_*.log')):
         runID=[]
         imgpair=[]
         choice=[]
+        side=[]
         outcome=[]
         matched=[]
         RT=[]
@@ -54,55 +55,59 @@ for file in glob.glob(os.path.join(basepath,'bevel_*.log')):
                 if x.find('at time= ')>1:
                     l_s=x.strip().split()
                     RT.append(l_s[5])
-                    
+                
+                if x.find('a keypress of ')>1:
+                    l_s=x.strip().split()
+                    side.append(l_s[12])
+                
                 if x.find('Level injecting via pump at address ')>-1:#find the tasty image
                     l_s=x.strip().split()
                     #print(l_s)
                     if l_s[7] == '1' and l_s[16] == 'a.jpg':
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('AB')
+                        #imgpair.append('AB')
                         choice.append('A')
                         outcome.append('reward')
                         matched.append('matched')
                     if l_s[7] == '1' and l_s[16] == 'c.jpg':
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('CD')
+                        #imgpair.append('CD')
                         choice.append('C')
                         outcome.append('reward')
                         matched.append('matched')
                     if l_s[7] == '1' and l_s[16] == 'e.jpg':
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('EF')
+                        #imgpair.append('EF')
                         choice.append('E')
                         outcome.append('reward')
                         matched.append('matched')
                     if l_s[7] == '1' and l_s[16] == 'b.jpg':
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('AB')
+                        #imgpair.append('AB')
                         choice.append('B')
                         outcome.append('reward')
                         matched.append('mismatched')
                     if l_s[7] == '1' and l_s[16] == 'd.jpg':
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('CD')
+                        #imgpair.append('CD')
                         choice.append('D')
                         outcome.append('reward')
                         matched.append('mismatched')
                     if l_s[7] == '1' and l_s[16] == 'f.jpg': 
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('EF')
+                        #imgpair.append('EF')
                         choice.append('F')
                         outcome.append('reward')
                         matched.append('mismatched')
                     if l_s[7] == '2' and l_s[16] == 'b.jpg':
                         subjID.append(sub)
-                        imgpair.append('AB')
+                        #imgpair.append('AB')
                         runID.append(run)
                         choice.append('B')
                         outcome.append('punish')
@@ -110,35 +115,35 @@ for file in glob.glob(os.path.join(basepath,'bevel_*.log')):
                     if l_s[7] == '2' and l_s[16] == 'd.jpg':  
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('CD')
+                        #imgpair.append('CD')
                         choice.append('D')
                         outcome.append('punish')
                         matched.append('matched')
                     if l_s[7] == '2' and l_s[16] == 'f.jpg':
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('EF')
+                        #imgpair.append('EF')
                         choice.append('F')
                         outcome.append('punish')
                         matched.append('matched')
                     if l_s[7] == '2' and l_s[16] == 'a.jpg':
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('AB')
+                        #imgpair.append('AB')
                         choice.append('A')
                         outcome.append('punish')
                         matched.append('mismatched')
                     if l_s[7] == '2' and l_s[16] == 'c.jpg':
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('CD')
+                        #imgpair.append('CD')
                         choice.append('C')
                         outcome.append('punish')
                         matched.append('mismatched')
                     if l_s[7] == '2' and l_s[16] == 'e.jpg':
                         subjID.append(sub)
                         runID.append(run)
-                        imgpair.append('EF')
+                        #imgpair.append('EF')
                         choice.append('E')
                         outcome.append('punish')
                         matched.append('mismatched')
@@ -149,12 +154,21 @@ for file in glob.glob(os.path.join(basepath,'bevel_*.log')):
                     print(l_s)
                     subjID.append(sub)
                     runID.append(run)
-                    imgpair.append('Miss')
+                    side.append('Miss')
+                    #imgpair.append('Miss')
                     choice.append('Miss')
                     outcome.append('Miss')
                     matched.append('Miss')
                     RT.append('Miss')
                 
+                if x.find('a.jpg at position')>-1:
+                    imgpair.append('AB')
+                
+                if x.find('c.jpg at position')>-1:
+                    imgpair.append('CD')
+
+                if x.find('e.jpg at position')>-1:
+                    imgpair.append('EF')
 
         #files2make=['task_log.csv']
         #mydict={}
@@ -170,6 +184,6 @@ for file in glob.glob(os.path.join(basepath,'bevel_*.log')):
       #              mydict[files] = path
            
     f_make=open(path, 'w')
-    for a,b,c,d,e,f,g in zip(subjID,runID,imgpair,choice,outcome,matched,RT):
-        f_make.write(str(a)+'\t'+str(b)+'\t'+str(c)+'\t'+str(d)+'\t'+str(e)+'\t'+str(f)+'\t'+str(g)+'\n')
+    for a,b,c,d,e,f,g,h in zip(subjID,runID,imgpair,choice,side,outcome,matched,RT):
+        f_make.write(str(a)+'\t'+str(b)+'\t'+str(c)+'\t'+str(d)+'\t'+str(e)+'\t'+str(f)+'\t'+str(g)+'\t'+str(h)+'\n')
     f_make.close()
