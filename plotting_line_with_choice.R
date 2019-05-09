95b21735a44b5dbe1e6ec124011b156a1744c69d
Jenny Gilbert <jennyrgilb@gmail.com>
2019-05-09 13:15:32 -0400

    adding r script for all participants

--- plotting_line_with_choice.R
+++ plotting_line_with_choice.R
@@ -8,9 +8,14 @@ library(psych)
 
 #Line plot of learned probabilities
 ## Need "long format" variable to plot all on the same graph via ggplot
-f1 = function(w) {
-setDT(w)
-DT.m1 = melt(w, measure.vars = c("a_learnedprob_correct", "b_learnedprob_correct","c_learnedprob_correct",
+
+setwd("/Users/jennygilbert/Documents/bevel_choice/by_participant_csv")
+myfiles <- list.files(pattern = ".csv")
+
+for (file in myfiles) {
+df = read.table(file, header=TRUE, sep=',')
+setDT(df)
+DT.m1 = melt(df, measure.vars = c("a_learnedprob_correct", "b_learnedprob_correct","c_learnedprob_correct",
                                     "d_learnedprob_correct", "e_learnedprob_correct", "f_learnedprob_correct"), 
              variable.name = "Shape", value.name = "learned_prob")
 #Get rid of the NA's and missing
@@ -20,21 +25,9 @@ df <- na.omit(DT.m1)
 #Plot learned probability 
 ggplot(df, aes(x=X, y=learned_prob)) + geom_line(aes(colour=Shape)) + 
   labs(title = "Learned Probability over time", x = "Trials", y = "Learned Probability", color = "Shapes")
+
+#Plot reinforcer responses
+counts <- prop.table(table(df$reinforcer_response))
+barplot(counts, main="Response Distribution", 
+    ylab="Percent of Responses", xlab='Response to Reinforcement', col = rainbow(5))
 }
-#Make a distribution of response to reinforcers
-f2 = function(w) {
-  pdf(file = 'plot', width = 4, height = 4)
-  counts <- prop.table(table(df$reinforcer_response))
-  barplot(counts, main="Response Distribution", 
-        ylab="Percent of Responses", xlab='Response to Reinforcement', col = rainbow(5))
-  dev.off() }
-
-setwd("/Users/jennygilbert/Documents/bevel_choice/by_participant_csv")
-mylist <- list.files(pattern = ".csv")
-
-for (i in mylist)
-  print(i)
-  df = read.table(i, header=TRUE, sep=',')
-  f1(df)
-  f2(df)
-  
